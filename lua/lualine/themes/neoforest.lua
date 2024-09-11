ce9e8dab2f695756bfb419c2f91231b118fdaa6d
Nikita Ganzha <nk8bnj@gmail.com>
2024-09-11 21:14:52 +0300

    color updated

--- lua/lualine/themes/neoforest.lua
+++ lua/lualine/themes/neoforest.lua
@@ -7,17 +7,17 @@ end
 
 return {
 	normal = {
-		a = { bg = palette.lightGrey[1], fg = palette.mainBlack[1], gui = "bold" },
+		a = { bg = palette.lightYellow[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.grey2[1] },
 		c = { bg = palette.bg1[1], fg = palette.lightGrey[1] },
 	},
 	insert = {
-		a = { bg = palette.lightGrey[1], fg = palette.mainBlack[1], gui = "bold" },
+		a = { bg = palette.mainAqua[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.grey2[1] },
 		c = { bg = palette.bg1[1], fg = palette.lightGrey[1] },
 	},
 	visual = {
-		a = { bg = palette.lightGrey[1], fg = palette.mainBlack[1], gui = "bold" },
+		a = { bg = palette.lightRed[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.grey2[1] },
 		c = { bg = palette.bg1[1], fg = palette.lightGrey[1] },
 	},
@@ -27,7 +27,7 @@ return {
 		c = { bg = palette.bg1[1], fg = palette.lightGrey[1] },
 	},
 	command = {
-		a = { bg = palette.lightGrey[1], fg = palette.mainBlack[1], gui = "bold" },
+		a = { bg = palette.lightOrange[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.grey2[1] },
 		c = { bg = palette.bg1[1], fg = palette.lightGrey[1] },
 	},
