1ad8262a1efbe09b91f7f37f2f8c8b11be52c4f6
Nikita Ganzha <nk8bnj@gmail.com>
2024-09-14 17:16:46 +0300

    color updated

--- lua/lualine/themes/neoforest.lua
+++ lua/lualine/themes/neoforest.lua
@@ -9,37 +9,37 @@ return {
 	normal = {
 		a = { bg = palette.lightYellow[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.grey2[1] },
-		c = { bg = palette.mainGrey[1], fg = palette.lightGrey[1] },
+		c = { bg = palette.darkGrey[1], fg = palette.lightGrey[1] },
 	},
 	insert = {
 		a = { bg = palette.lightAqua[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.grey2[1] },
-		c = { bg = palette.mainGrey[1], fg = palette.lightGrey[1] },
+		c = { bg = palette.darkGrey[1], fg = palette.lightGrey[1] },
 	},
 	visual = {
 		a = { bg = palette.lightPurple[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.grey2[1] },
-		c = { bg = palette.mainGrey[1], fg = palette.lightGrey[1] },
+		c = { bg = palette.darkGrey[1], fg = palette.lightGrey[1] },
 	},
 	replace = {
 		a = { bg = palette.lightRed[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.grey2[1] },
-		c = { bg = palette.mainGrey[1], fg = palette.lightGrey[1] },
+		c = { bg = palette.darkGrey[1], fg = palette.lightGrey[1] },
 	},
 	command = {
 		a = { bg = palette.lightOrange[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.grey2[1] },
-		c = { bg = palette.mainGrey[1], fg = palette.lightGrey[1] },
+		c = { bg = palette.darkGrey[1], fg = palette.lightGrey[1] },
 	},
 	terminal = {
 		a = { bg = palette.lightGrey[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.grey2[1] },
-		c = { bg = palette.mainGrey[1], fg = palette.lightGrey[1] },
+		c = { bg = palette.darkGrey[1], fg = palette.lightGrey[1] },
 	},
 
 	inactive = {
-		a = { bg = palette.mainGrey[1], fg = palette.lightGrey[1] },
-		b = { bg = palette.mainGrey[1], fg = palette.lightGrey[1] },
-		c = { bg = palette.mainGrey[1], fg = palette.lightGrey[1] },
+		a = { bg = palette.darkGrey[1], fg = palette.lightGrey[1] },
+		b = { bg = palette.darkGrey[1], fg = palette.lightGrey[1] },
+		c = { bg = palette.darkGrey[1], fg = palette.lightGrey[1] },
 	},
 }
