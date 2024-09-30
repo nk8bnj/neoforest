f04a6e844719f86dff28e89de392aa7d53f0bef6
Nikita Ganzha <nk8bnj@gmail.com>
2024-09-30 15:27:30 +0300

    update color

--- lua/lualine/themes/neoforest.lua
+++ lua/lualine/themes/neoforest.lua
@@ -8,7 +8,7 @@ end
 return {
 	normal = {
 		a = { bg = palette.darkBlue[1], fg = palette.mainBlack[1], gui = "bold" },
-		b = { bg = palette.bg1[1], fg = palette.lightGrey[1] },
+		b = { bg = palette.bg3[1], fg = palette.lightGrey[1] },
 		c = { bg = palette.bg1[1], fg = palette.lightGrey[1] },
 	},
 	insert = {
