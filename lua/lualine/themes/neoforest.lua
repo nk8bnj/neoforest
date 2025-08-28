8f6c992af98939a39cfdb2fb47417d2607a0f269
Nikita Ganzha <nk8bnj@gmail.com>
2025-08-28 16:57:28 +0300

    Update colors

--- lua/lualine/themes/neoforest.lua
+++ lua/lualine/themes/neoforest.lua
@@ -12,7 +12,7 @@ return {
 		c = { bg = palette.bg1[1], fg = palette.lightGrey2[1] },
 	},
 	insert = {
-		a = { bg = palette.darkBlue[1], fg = palette.mainBlack[1], gui = "bold" },
+		a = { bg = palette.blue[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.lightGrey[1] },
 		c = { bg = palette.bg1[1], fg = palette.lightGrey2[1] },
 	},
@@ -27,7 +27,7 @@ return {
 		c = { bg = palette.bg1[1], fg = palette.lightGrey2[1] },
 	},
 	command = {
-		a = { bg = palette.lightYellow[1], fg = palette.mainBlack[1], gui = "bold" },
+		a = { bg = palette.yellow[1], fg = palette.mainBlack[1], gui = "bold" },
 		b = { bg = palette.bg3[1], fg = palette.lightGrey[1] },
 		c = { bg = palette.bg1[1], fg = palette.lightGrey2[1] },
 	},
